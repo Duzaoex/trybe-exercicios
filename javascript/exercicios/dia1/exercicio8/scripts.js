acc50309d85a77faabc2bb924179b296e396ef1a
duzao <eduardoneriscarneiro@gmail.com>
2023-05-02 18:25:30 -0300

    exercicio8

---
+++ javascript/exercicios/dia1/exercicio8/scripts.js
@@ -0,0 +1,8 @@
+let prova = 10;
+let trabalho = 9;
+
+if (prova > trabalho) {
+   console.log('A nota da prova é maior que do trabalho');
+} else {
+    console.log('Nota do trabalho é maior que da prova');
+}
