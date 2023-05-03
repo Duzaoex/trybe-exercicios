c0b28d186bd67b7f0696febef2b6d2ccbcc6ead8
duzao <eduardoneriscarneiro@gmail.com>
2023-05-03 15:19:03 -0300

    exercicio1

---
+++ javascript/exercicios/dia2/exercicio1/scripts.js
@@ -0,0 +1,12 @@
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+console.log(menu1[1]);
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+console.log(menu2[2]);
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push('contato');
+
+console.log(menu3);
