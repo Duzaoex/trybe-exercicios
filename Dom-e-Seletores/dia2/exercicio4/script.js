93a17b8823a5d7400a2710e936517d7979046b26
Eduardo Neris Carneiro <92535485+Duzaoex@users.noreply.github.com>
2023-05-30 01:07:02 -0300

    Delete Dom-e-Seletores directory

--- Dom-e-Seletores/dia2/exercicio4/script.js
+++
@@ -1,5 +0,0 @@
-const pai = document.getElementById('where-are-you');
-
-const filho = document.getElementsByTagName('h2')[1];
-if (filho.innerText.includes('Elemento que você deve remover'));
-pai.removeChild(filho);
